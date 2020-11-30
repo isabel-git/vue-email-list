e48576efd0e5b3c314e7115dab51192a6124a6e0
sourceCodeISA <isabel.gogov1@libero.it>
2020-11-30 22:47:50 +0100

    bonus: genero email api al click

--- js/script.js
+++ js/script.js
@@ -5,15 +5,31 @@ var app = new Vue({
   data: {
     email: []
   },
-  mounted: function () {
-    for (let index = 0; index < 10; index++) {
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      .then(risposta => {
+  // genero email usando api
+  // mounted: function () { 
+  //   for (let index = 0; index < 10; index++) {
+  //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //     .then(risposta => {
 
-        this.email.push(risposta.data.response)
+  //       this.email.push(risposta.data.response)
 
-      });
+  //     });
 
+  //   }
+  // }
+
+// BONUS: genero email api al click
+  methods: {
+
+    clickMail: function () {
+      for (let index = 0; index < 10; index++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(risposta => {
+
+          this.email.push(risposta.data.response);
+
+        })
+      }
     }
-  }
+  },
 });
